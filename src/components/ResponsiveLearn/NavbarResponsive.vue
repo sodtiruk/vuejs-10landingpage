b1f85fc9991da4cf029d833a87e588764d924df8
sutthirak.s <sutthirak.s@smartblocktech.ai>
2024-12-16 17:03:16 +0700

    feat: add NavbarResponsive.vue and fixed Navbar on top

--- src/components/ResponsiveLearn/NavbarResponsive.vue
+++ src/components/ResponsiveLearn/NavbarResponsive.vue
@@ -1,12 +1,254 @@
 <template>
-    <div class="grid">
-        <div class="box"></div> 
-        <div class="box"></div> 
-        <div class="box"></div> 
-        <div class="box"></div> 
-        <div class="box"></div> 
-        <div class="box"></div> 
-        <div class="box"></div> 
+    <nav>
+       <div class="container">
+            <div class="content">
+                <div class="left">
+                    <img src="/src/assets/apple.svg" alt="">
+                    <h1>TalkNotes</h1>
+                </div>
+                <div class="middle">
+                    <ul>
+                        <li><a href="">Try it</a></li>
+                        <li><a href="">How it works</a></li>
+                        <li><a href="">use cases</a></li>
+                        <li><a href="">Pricing</a></li>
+                        <li><a href="">FAQ</a></li>
+                    </ul>
+                </div>
+                <div class="right">
+                    <button class="login">Login</button>
+                    <button class="get"> Get Talknotes + </button>
+                </div>
+            </div>
+       </div> 
+    </nav>
+    <div class="test">
+        xxLorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, pariatur?
     </div>
 </template>
 <script>
@@ -19,29 +261,91 @@ export default defineComponent({
 })
 </script>
 <style scoped>
-    
-.grid {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-}    
 
-.box {
-    background-color: blue;
-    border: 1px solid red;
-    padding: 1rem;
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+
+* {
+    list-style: none;
+    text-decoration: none;
+    font-family: "Roboto", sans-serif;
 }
 
-@media screen and (max-width: 600px){
+nav {
+    padding: 1rem 1.5rem;
+    position: fixed;
+    top: 0;
+    /* overflow: hidden; */
+    background-color: white;
+    width: 100%;
+}
 
-    .grid {
-        grid-template-columns: repeat(2, 1fr);
-    }
+.container {
+    max-width: 1040px;
+    margin: 0 auto;
+}
 
-    .box {
-        background-color: blueviolet;
-    }
+.content {
+    display: flex;
+    justify-content: space-between;
+}
+
+.content .left {
+    display: flex;
+    align-items: center;
+}
+
+.content .left img {
+    width: 40px;
+    margin-right: 1rem;
+}
+
+.content .middle {
+    display: flex;
+    align-items: center;
+}
+
+.content .middle ul {
+    display: flex;
+    align-items: center;
+}
+
+.content .middle ul li a {
+    padding: 1rem;
+    font-size: 18px;
+    color: gray;
+}
+
+.content .right {
+    display: flex;
+    gap: 1rem;
+}
+
+.content .right .login {
+    padding: .4rem 1.1rem;
+    border-radius: 8px;
+    font-size: 14px;
+    border: none;
+    background-color: #eef2fd;
+    color: #052784;
+} 
 
 
+.content .right .login:hover{ 
+    background-color: #c9d6f9;
+}
+
+
+.content .right .get { 
+    border: none;
+    padding: .4rem 1.1rem;
+    font-size: 14px;
+    border-radius: 8px;
+    background-color: #4F46E5;
+    color: white;
+}
+
+.test {
+    margin-top: 5rem;
 }
 
 
